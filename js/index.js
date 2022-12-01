26fcf9c073aa6c50d505bbbd8d9b360648d576d1
Seunggyu <seunggyul14@icloud.com>
2022-11-30 18:58:33 -0800

    commit tree b12fe854013bc7664746c015b4f9a4c3be7e55a8

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    // deliverable one
+    // get book data - DONE
+    // get title of books
+
+    fetch("http://localhost:3000/books")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+        data.forEach((bookObj) => {
+            console.log(bookObj.title)
+        })
+    })
+    
+    console.log("hello");
+    console.log("Hi");
+});
